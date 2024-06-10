114c37dac722627ab394300808f8740245e21424
GabrielSilvaCosta <bob@>
2024-06-10 15:51:58 -0300

    feat unit testes planet solar

---
+++ src/components/__tests__/MissionsList.test.js
@@ -0,0 +1,15 @@
+import { mount } from "@vue/test-utils";
+import { describe, expect, it } from "vitest";
+import Missions from '../../data/Missions';
+
+describe('Missions data', () => {
+    it('should have an array of missions', () => {
+      expect(Array.isArray(Missions)).toBe(true);
+    });
+
+    it('should have 20 missions', () => {
+      expect(Missions.length).toBe(20);
+    });
+  });
+
+
